81272ecdce9098f67019446c8aed4f63085b8a59
Bruce Alves <brucealves800@gmail.com>
2025-11-23 21:17:40 -0300

    testando .env

--- BackEnd/Policia-Service/server.js
+++ BackEnd/Policia-Service/server.js
@@ -1,6 +1,6 @@
+import 'dotenv/config';
 import express from 'express';
 import policiaRoutes from "./src/routes/policia.routes.js";
-
 
 const app = express();
 app.use(express.json());
@@ -10,4 +10,4 @@ app.use("/policia", policiaRoutes);
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log('Servidor da policia rodando!');
-})
+});
