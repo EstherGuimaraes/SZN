56697f3e07a93a17ff289a3a9e15385144c04de5
Italo Adriano <italogleray@gmail.com>
2025-11-26 09:46:56 -0300

    ARRUMEI A API

--- login_mysql/src/config/db.js
+++ login_mysql/src/config/db.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.DB_HOST);
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
